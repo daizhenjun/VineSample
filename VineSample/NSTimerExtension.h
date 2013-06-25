8a0bee5c53e6d41292a4eec8eb03c7598f4a7072
daizhenjun <daizhj@gmail.com>
2013-06-25 11:07:58 +0800

    first commit

---
+++ VineSample/NSTimerExtension.h
@@ -0,0 +1,13 @@
+//
+//  NSTimerExtension.h
+//  VineSample
+//
+//  Created by 代 震军 on 13-6-18.
+//  Copyright (c) 2013年 代 震军. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSTimerExtension : NSObject
+
+@end
---
+++ VineSample/NSTimerExtension.m
@@ -0,0 +1,13 @@
+//
+//  NSTimerExtension.m
+//  VineSample
+//
+//  Created by 代 震军 on 13-6-18.
+//  Copyright (c) 2013年 代 震军. All rights reserved.
+//
+
+#import "NSTimerExtension.h"
+
+@implementation NSTimerExtension
+
+@end
